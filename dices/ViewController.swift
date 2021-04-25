fbe884a1d2982053af65e756f75c02610077059a
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-04-26 00:31:03 +0800

    Initial Commit

---
+++ dices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dices
+//
+//  Created by 簡吟真 on 2021/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
